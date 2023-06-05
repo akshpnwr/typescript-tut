1004736c74e440ea4a6fb5c8ca191b485302199e
akshpnwr <akashpanwar9976@gmail.com>
2023-06-05 11:49:43 +0530

    typescripting an object

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
+import { useState } from 'react'
 import './App.css'
+
+type PostProps = {
+  id: number,
+  title: string
+}[];
+
+function getData(): PostProps {
+  return [{id: 1, title: 't1'}, {id: 2, title: 't2'}, {id: 3, title: 't3'}];
+}
 
 function App() {
 
+  const [title, setTitle] = useState<string | null>("Title");
+
+  const changeTitle = () => {
+    setTitle("New title");
+  }
+
+  const data = getData();
+
   return (
-    <h1>
-      Typescript Demo
+    <>
+    <h1 onClick={changeTitle}>
+      {title}
     </h1>
+    <h1>Typescripting an Object</h1>
+    {data.map(item => 
+      <div>
+        <h2>{item.id}</h2>
+        <p>{item.title}</p>
+      </div>
+    )  
+  }
+    </>
   )
 }
 
