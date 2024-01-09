301b0e7a3d6cc8fc860ba00b8aa5387638551036
akshpnwr <akashpanwar9976@gmail.com>
2024-01-09 17:57:54 +0530

    Creating the Company class

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello akash');
+import {User} from './User.ts';
+import {Company} from './Company.ts';
+
+const user = new User();
+
+const company = new Company();
+
+console.log('user', user);
+console.log('company', company);
