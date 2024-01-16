50bdc1b2fefba83f42e99c92c438b93a2c363e12
akshpnwr <akashpanwar9976@gmail.com>
2024-01-16 20:03:36 +0530

    Add action types and interfaces to repositoriesReducer.ts

--- redux-typescript-demo/src/state/repositoriesReducer.ts
+++ redux-typescript-demo/src/state/repositoriesReducer.ts
@@ -1,13 +1,29 @@
+enum ActionType {
+    SEARCH_REPOSITORIES = 'search_repositories',
+    SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+    SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
+}
 interface RepositoriesState {
     data: string[],
     loading: boolean,
     error: string | null
 }
 
-interface Action {
-    type: string,
-    payload?: any
+interface SearchAction {
+    type: ActionType.SEARCH_REPOSITORIES,
 }
+
+interface SuccessAction {
+    type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+    payload: string[]
+}
+
+interface ErrorAction {
+    type: ActionType.SEARCH_REPOSITORIES_ERROR,
+    payload: string
+}
+
+type Action = SearchAction | SuccessAction | ErrorAction;
 
 // Define the initial state
 const initialState = {
@@ -20,11 +36,11 @@ const initialState = {
 const reducer = (state: RepositoriesState = initialState, action: Action): RepositoriesState => {
     switch (action.type) {
         // Add your action types and corresponding state updates here
-        case 'search_repositories':
+        case ActionType.SEARCH_REPOSITORIES:
             return  {loading: true, error: null, data: []}
-        case 'search_repositories_success':
+        case ActionType.SEARCH_REPOSITORIES_SUCCESS:
             return {loading: false, error: null, data: action.payload }        
-        case 'search_repositories_error':
+        case ActionType.SEARCH_REPOSITORIES_ERROR:
             return {loading: false, error: action.payload, data: []}
         default:
             return state;
